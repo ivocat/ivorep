0fd4d7a324c904fff01626e60b67f4dd8fbf7462
ivocat <35920936+ivocat@users.noreply.github.com>
2018-02-02 11:05:49 +0300

    Added rounding, cut out prints, chained methods
    While checking if the triangle is isosceles the values are rounded to 3 decimal numbers
    Extra puts lines containing tracing info are deleted
    Some methods are now chained in one line instead of two
    'Isoright' variable is now eliminated since there is only time where it is needed

--- task_1/right_triangle.rb
+++ task_1/right_triangle.rb
@@ -1,7 +1,6 @@
 puts "Введите длины трёх сторон треугольника через запятую"
 sides = gets.chomp.gsub(" ","").split(",")
-sides.map! {|s| s.to_f}
-sides.sort!
+sides.sort!.map! {|s| s.to_f}
 
 if sides.length < 3
   puts "Вы ввели меньше трёх сторон"
@@ -10,22 +9,17 @@ elsif sides.length > 3
   puts "Вы ввели больше трёх сторон"
   exit
 elsif sides.inject(:*) == 0
-  puts "Такого треугольника быть не может"
+  puts "Вы ввели неправильные данные"
   exit
 end
 
 isosceles = (sides[0] == sides [1]) || (sides[0] == sides[2]) || (sides[1] == sides[2])
-right = sides[2]**2 == ( sides[0]**2 + sides[1]**2 )
-isoright = isoright && right
+right = (sides[2]**2).round(3) == ( sides[0]**2 + sides[1]**2 ).round(3)
 
-if isoright
+if isosceles && right
   puts "Трегуольник прямоугольный и равнобедренный"
 elsif right
   puts "Треугольник прямоугольный"
 else
   puts "Треугольник не прямоугольный"
 end
-
-a = Math.sqrt(sides[0]**2 + sides[1]**2)
-sides.each {|s| puts s}
-puts a
