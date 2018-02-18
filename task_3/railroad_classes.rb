e62d93789b0361708f8d49219eb5fedb79223bf8
ivocat <alyrn.n@gmail.com>
2018-02-18 17:46:21 +0300

    Finished the rest in the task

--- task_3/railroad_classes.rb
+++ task_3/railroad_classes.rb
@@ -1,35 +1,104 @@
 class Station
-  
-  @@trains_on_station = {}
-  
-  def initialize(station_name)
-    @station_name = station_name
+  attr_reader :trains_list, :name
+
+  def initialize(name)
+    @name = name
+    @trains_list = []
   end
-  
-  def acommodate(train_num, train_type)
-    @train_instance = train_instance
-    @train_type = train_type
-    trains_on_station[@train_instance] = train_type
-    puts "Train #{train_num} has arrived at the station."
+
+  def accommodate(train)
+    @trains_list << train
   end
-  
-  def stationed_trains
-    @@trains_on_station.each do |num, type|
-      puts "List of trains on the station #{@station_name}:" if @@trains_on_station.length != 0
-      puts "#{num}: #{type}"
-    end
+
+  def depart(train)
+    @trains_list.delete(train)
   end
-  
-  def stationed_trains_by_type(desired_type)
-    @@trains_on_station.each do |num,type|
-      puts num if type == desired_type #доделать
-    end
-  end
-  
-  def depart_train(train_num)
-    @@trains_on_station.delete(train_num)
-    puts "Train #{train_num}" has left the station.
-    @@trains_on_station
+
+  def trains_list_by_type(desired_type)
+    @trains_list.select {|train| train.type == desired_type}
   end
 end
 
+class Route
+  attr_reader :route
+
+  def initialize(departure_station, terminal_station)
+    @route = [departure_station, terminal_station]
+  end
+
+  def add_train_stop(station, previous_station)
+    previous_num = @route.rindex(previous_station)
+    if previous_num != nil && previous_station != @route[-1]
+      @route.insert(previous_num + 1,station)
+    end
+  end
+
+  def remove_train_stop(station)
+    if station != @route[0] && station != @route[-1]
+      @route.delete(station)
+    end
+  end
+end
+
+class Train
+  attr_accessor :cars_number, :type, :number, :speed
+
+  def initialize(number, type, cars_number)
+    @number = number.to_s
+    @type = type.to_sym
+    @cars_number = cars_number.to_i
+    @speed = 0
+    @route = []
+    @location = 0
+  end
+
+  def speed_change(speed_value)
+    @speed += speed_value
+    @speed = 0 if @speed < 0
+  end
+
+  def full_stop
+    @speed = 0
+  end
+
+  def car_add
+    @cars_number += 1 if @speed == 0
+  end
+
+  def car_remove
+    @cars_number -= 1 if @speed == 0
+  end
+
+  def get_route(route_object)
+    @route = route_object.route
+    @route[0].accommodate(self)
+  end
+
+  def move_forward
+    unless @location == @route.length - 1
+      @route[@location].depart(self)
+      @location += 1
+      @route[@location].accommodate(self)
+    end
+  end
+
+  def move_back
+    unless @location == 0
+      @route[@location].depart(self)
+      @location -= 1
+      @route[@location].accommodate(self)
+    end
+  end
+
+  def current_station
+    @route[@location]
+  end
+
+  def previous_station
+    @route[@location-1] unless @location == 0
+  end
+
+  def next_station
+    @route[@location+1] unless @location == @route.length - 1
+  end
+end
