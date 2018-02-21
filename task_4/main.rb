dc763c8538c3674705440edb76c521c67681cba5
ivocat <alyrn.n@gmail.com>
2018-02-21 21:18:43 +0300

    Started menu elaboration

--- task_4/main.rb
+++ task_4/main.rb
@@ -1,9 +1,16 @@
 require_relative "station"
-require_relative "cargo_car"
-require_relative "cargo_train"
-require_relative "passenger_car"
-require_relative "passenger_train"
 require_relative "route"
 require_relative "station"
 require_relative "train"
 require_relative "train_car"
+require_relative "cargo_car"
+require_relative "cargo_train"
+require_relative "passenger_car"
+require_relative "passenger_train"
+require_relative "menu"
+
+menu = Menu.new
+
+loop do
+  menu.execute
+end
