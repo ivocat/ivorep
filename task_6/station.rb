ebbc18c94646daec1c061496aa5eeed90067b9ae
ivocat <35920936+ivocat@users.noreply.github.com>
2018-03-15 14:43:23 +0300

    Added "all" method for station

--- task_6/station.rb
+++ task_6/station.rb
@@ -1,9 +1,15 @@
 class Station
   attr_reader :trains, :name
+  @@stations = []
 
+  def self.all
+    @@stations
+  end
+  
   def initialize(name)
     @name = name
     @trains = []
+    @@stations << self
   end
 
   def accommodate(train)
