554201de123c92d01f2d3615d61a5ccce60cd704
ivocat <35920936+ivocat@users.noreply.github.com>
2018-03-16 18:14:07 +0300

    Removed obsolete model instance variable

--- task_6/train_car.rb
+++ task_6/train_car.rb
@@ -2,9 +2,8 @@ require_relative "manufacturer"
 
 class TrainCar
   include Manufacturer
-  attr_reader :model #добавил модель вагона, чтобы не было скучно
 
   def initialize(model)
-    @model = model.to_sym
+    set_manufacturer(model)
   end
 end
