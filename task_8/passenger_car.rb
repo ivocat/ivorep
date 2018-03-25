08716e8b0e6556fd5705c375f2cfac7b50287ebf
ivocat <alyrn.n@gmail.com>
2018-03-25 23:17:42 +0300

    Added passenger car methods

--- task_8/passenger_car.rb
+++ task_8/passenger_car.rb
@@ -1,4 +1,20 @@
 require_relative "train_car"
 
 class PassengerCar < TrainCar
+  attr_reader :seats_total, :seats_taken
+  @seats_total = 0
+  @seats_taken = 0
+
+  def initialize(model, seats_total)
+    self.manufacturer = model
+    @seats_total = seats_total
+  end
+
+  def occupy_seat
+    @seats_taken += 1
+  end
+
+  def seats_free
+    @seats_total - @seats_taken
+  end
 end
