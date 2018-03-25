95d7cf4549e2c1fdddb3cee157f67f31247bbe4e
ivocat <alyrn.n@gmail.com>
2018-03-26 00:17:34 +0300

    Fixed cargo car methods

--- task_8/cargo_car.rb
+++ task_8/cargo_car.rb
@@ -2,16 +2,16 @@ require_relative "train_car"
 
 class CargoCar < TrainCar
   attr_reader :capacity, :capacity_taken
-  @capacity = 0
-  @capacity_taken = 0
+  @capacity = 0.0
+  @capacity_taken = 0.0
 
   def initialize(model, capacity)
     self.manufacturer = model
     @capacity = capacity
   end
 
-  def occupy_space
-    @capacity_taken += 1
+  def occupy_space(volume)
+    @capacity_taken += volume
   end
 
   def capacity_remaining
