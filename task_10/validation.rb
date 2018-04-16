31328bb830337b6f8816b2c0df4c58a75db4556f
ivocat <alyrn.n@gmail.com>
2018-04-16 23:29:03 +0300

    Implemented comments

--- task_10/validation.rb
+++ task_10/validation.rb
@@ -6,6 +6,7 @@ module Validation
 
   module ClassMethods
     attr_reader :validations
+    attr_accessor :latest_error
 
     def validate(attribute_name, validation_type, arg = nil)
       @validations ||= {}
@@ -18,44 +19,37 @@ module Validation
     def valid?
       validate!
       true
-    rescue RuntimeError => e
-      puts "не прошла — #{e.message}"
+    rescue NoMethodError, RuntimeError => e
+      self.class.latest_error = e
       false
     end
 
     protected
 
     def validate!
-      self.class.validations.each do |attribute_name, validation_array|
-        validation_array.each do |validation_hash|
-          begin
-            case validation_hash[:validation_type]
-            when :presence
-              validate_presence(attribute_name, self.instance_variable_get("@#{attribute_name}"))
-            when :format
-              validate_format(attribute_name, self.instance_variable_get("@#{attribute_name}"), validation_hash[:arg])
-            when :type
-              validate_type(attribute_name, self.instance_variable_get("@#{attribute_name}"), validation_hash[:arg])
-            else
-              raise "неправильно задана валидация: #{validation_type}, параметр: #{arg}"
-            end
-          # rescue RuntimeError => err
-          #   print "\nВалидация атрибута #{attribute_name} у ", self.instance_variable_get("@#{attribute_name}"), " не прошла: #{err.message}."
+      unless self.class.instance_variable_get("@validations").nil?
+        self.class.validations.each do |attribute_name, validation_array|
+          attribute = self.instance_variable_get("@#{attribute_name}")
+          validation_array.each do |validation_hash|
+            self.send("validate_#{validation_hash[:validation_type]}", \
+                      attribute_name, \
+                      attribute, \
+                      validation_hash[:arg])
           end
         end
       end
     end
 
-    def validate_presence(attribute_name, attribute)
-      raise "атрибут #{attribute_name} не существует или пуст" if attribute == "" || attribute.nil?
+    def validate_presence(attribute_name, attribute, arg = nil)
+      raise "атрибут #{attribute_name} не существует или пуст" if attribute.to_s.empty?
     end
 
-    def validate_format(attribute_name, attribute, arg)
-      raise "неправильный формат атрибута #{attribute_name} ('#{attribute}')" if attribute !~ arg
+    def validate_format(attribute_name, attribute, string_format)
+      raise "неправильный формат атрибута #{attribute_name} ('#{attribute}')" if attribute !~ string_format
     end
 
-    def validate_type(attribute_name, attribute, arg)
-      raise "класс атрибута #{attribute_name} не #{arg}" if !attribute.is_a?(arg)
+    def validate_type(attribute_name, attribute, klass)
+      raise "класс атрибута #{attribute_name} не #{klass}" unless attribute.is_a?(klass)
     end
   end
 end
